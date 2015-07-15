869b5f7082202220bf5a5612952d1b4f6a59bf9b
Dinis Rosario <dinis.rosario@gmail.com>
2015-07-15 19:37:35 +0000

    Modify aws_events_app start and stop to print dummy messages

--- src/aws_events_app.erl
+++ src/aws_events_app.erl
@@ -13,4 +13,4 @@ start(_StartType, _StartArgs) ->
     aws_events_sup:start_link().
 
 stop(_State) ->
-    ok.
+    aws_events_sup:stop().
--- src/aws_events_sup.erl
+++ src/aws_events_sup.erl
@@ -3,7 +3,7 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/0, stop/0]).
 
 %% Supervisor callbacks
 -export([init/1]).
@@ -18,10 +18,15 @@
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
+stop() ->
+    io:format("Bye bye... ~n"),
+    ok.
+
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
 init([]) ->
+    io:format("Running....~n"),
     {ok, { {one_for_one, 5, 10}, []} }.
 
