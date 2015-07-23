465a59149417304ef09d90eae4c8dff3da6289d2
Dinis Rosario <dinis.rosario@gmail.com>
2015-07-23 14:03:43 +0100

    Implement s3_uploader

--- src/s3_uploader.erl
+++ src/s3_uploader.erl
@@ -17,20 +17,39 @@ stop() ->
 
 %EVENT LOOP BEHAVIOUR
 init([]) ->
-  {ok, []}.
+  {ok, AWSAccessId} = application:get_env(aws_events, aws_access),
+  {ok, AWSSecret} = application:get_env(aws_events, aws_secret),
+  erlcloud_s3:configure(AWSAccessId, AWSSecret),
+  {ok, Bucket} = application:get_env(aws_events, aws_s3_bucket),
+  %%if bucket doesn't exist, this process fail here!
+  lager:info("Getting info from S3 bucket ~p", [Bucket]),
+  Location = erlcloud_s3:get_bucket_attribute(Bucket, location),
+  lager:info("S3 bucket ~p from ~p", [Bucket, Location]),
+  {ok, Bucket}.
 
-handle_event(File, State) ->
-  lager:info("s3_uploader: uploading ~p",[File]),
-  {ok, State}.
+handle_event(File, Bucket) ->
+  lager:info("s3_uploader: Reading file ~p",[File]),
+  try file:read_file(File) of
+    {ok, Data} ->
+      lager:info("s3_uploader: uploading ~p",[File]),
+      erlcloud_s3:put_object(Bucket, filename:basename(File), Data),
+      lager:info("s3_uploader: uploaded ~p",[File]);
+    Error ->
+      lager:error("Can't upload ~p~nError~p",[File, Error])
+  catch
+    Error ->
+      lager:error("Can't upload ~p~nError~p",[File, Error])
+  end,
+  {ok, Bucket}.
 
-handle_call(_, State) ->
-  {ok, ok, State}.
+handle_call(_, Bucket) ->
+  {ok, ok, Bucket}.
 
-handle_info(_, State) ->
-  {ok, State}.
+handle_info(_, Bucket) ->
+  {ok, Bucket}.
 
-code_change(_OldVsn, State, _Extra) ->
-  {ok, State}.
+code_change(_OldVsn, Bucket, _Extra) ->
+  {ok, Bucket}.
 
 terminate(_Reason, _State) ->
   ok.
